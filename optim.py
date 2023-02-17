d40b75032fb18e68a3d43e0d91a3409fe2a22c12
ApollineRiet <44115960+ApollineRiet@users.noreply.github.com>
2023-02-17 14:55:34 +0000

    Modification optim sur PC CI

--- optim.py
+++ optim.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import pulp
-from geopy.distance import great_circle
-
-
 
 # Données
 
 print("-----  Création des données   -----")
 
+TAILLE = 1000
+
 # vols notés i, de 1 à n
 flights = pd.read_csv('flights_and_emissions.csv')
+flights = flights.head(TAILLE)
 n = len(flights)
 
 # types d'avions notés j, de 1 à m
@@ -21,7 +21,7 @@ avions.insert(len(avions.columns),'N0', pd.Series(np.ones(m) * 10, dtype=int))
 avions.insert(len(avions.columns),'Capacite', pd.Series(np.ones(m) * 200, dtype=int))
 N_avions = np.empty(m, dtype=object)
 
-# Extraction de tableaux utiles : 
+# Extraction de tableaux utiles :
 AC_type_vol = flights['AC Type']
 lat_departs = flights['ADEP Latitude']
 lon_departs = flights['ADES Longitude']
@@ -49,15 +49,12 @@ passagers_init = np.ones(n) * 200 # en vrai, à aller chercher selon type d'avio
 # nombre de passagers variable
 passagers = np.empty((n,m), dtype=object)
 
-# Tableau CO2 total - PROVISOIRE, à remplacer pas
-#CO2 = np.load("CO2.npy")
-CO2 = np.empty((n,m))
-for i in range(n):
-    for j in range(m):
-        CO2[i,j] = 7000 
+# Tableau CO2 total
+CO2 = np.load("CO2.npy")
+CO2 = CO2[:n]
 
-place_train = (82000000 / 0.314 - 82000000) / 12
-#place_train = 200 * n / 2
+#PLACE_TRAIN = (82000000 / 0.314 - 82000000) / 12
+PLACE_TRAIN = 200 * n / 2
 
 CO2_depart = np.sum(CO2_init)
 
@@ -68,8 +65,8 @@ prob = pulp.LpProblem("Optim", pulp.LpMinimize)
 # Variables de décision
 for i in range(n):
     for j in range(m):
-        statut_vol[i,j] = pulp.LpVariable('statut_{}'.format(i),cat=pulp.LpBinary)
-        passagers[i,j] = pulp.LpVariable('passagers_{}'.format(i), cat=pulp.LpInteger)
+        statut_vol[i,j] = pulp.LpVariable('statut_{}_{}'.format(i,j),cat=pulp.LpBinary)
+        passagers[i,j] = pulp.LpVariable('passagers_{}_{}'.format(i,j), cat=pulp.LpInteger)
 for j in range(m):
     N_avions[j] = pulp.LpVariable('dispo_{}'.format(j), cat=pulp.LpInteger)
 
@@ -80,8 +77,8 @@ for i in range(n):
         #prob += np.sum(passagers,axis=1)[i] <= passagers_init[i]
         prob += passagers[i,j] <= statut_vol[i,j] * avions['Capacite'][j]
         prob += statut_vol[i].sum() <= 1
-    
-prob += (np.sum(passagers,axis=1) - passagers_init).sum() <= place_train
+
+prob += (np.sum(passagers,axis=1) - passagers_init).sum() <= PLACE_TRAIN
 
 for j in range(m):
     prob += N_avions[j] >= avions['N0'][j]
@@ -94,18 +91,20 @@ prob += pulp.lpSum(np.multiply(CO2, statut_vol))
 status = prob.solve()
 print("Status: ",pulp.LpStatus[status])
 
+"""
 print("Variables: ", end=' ')
 for i in range(n):
-    print(pulp.value(statut_vol[i]), end=' ')
+    print(pulp.value(statut_vol[i,j]), end=' ')
 print()
+"""
 
-statut_vol_fin = np.empty(n)
+statut_vol_fin = np.empty((n,m))
 for i in range(n):
-    statut_vol_fin[i] = pulp.value(statut_vol[i])
+    for j in range(m):
+        statut_vol_fin[i,j] = pulp.value(statut_vol[i,j])
 
 CO2_fin = np.multiply(CO2,statut_vol_fin).sum()
 
 print("Début : " + str(CO2_depart))
 print("Fin : " + str(CO2_fin))
 print("Delta CO2 économisé : " + str(CO2_depart-CO2_fin))
-
